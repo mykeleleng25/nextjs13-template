fa6a0cb427846902dcff7daea352e9dac6596500
mykeleleng25 <mykeleleng25axie@email.com>
2023-06-21 21:24:46 +0800

    feat(shadcn): initial commit

---
+++ app/(information)/layout.tsx
@@ -0,0 +1,12 @@
+export default function InformationLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <>
+      <h1>Information</h1>
+      {children}
+    </>
+  )
+}
---
+++ app/@organization/page.tsx
@@ -0,0 +1,3 @@
+export default function Organization() {
+  return <h1>This is Organization</h1>
+}
